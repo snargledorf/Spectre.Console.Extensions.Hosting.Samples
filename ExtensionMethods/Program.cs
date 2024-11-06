34461c461d1bbe6017f7c966a1c9bca68ada90fb
Ryan Esteves <snargledorf@users.noreply.github.com>
2024-11-06 11:46:22 -0500

    Upgrade to SpectreConsoleHost 1.1.0
    Added example app for extension methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Spectre.Console.Cli;
+using Spectre.Console.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSpectreConsole<DefaultCommand>(args, configurator =>
+{
+    configurator.AddCommand<ExampleCommand>("example");
+});
+            
+await builder.Build().RunAsync();
+
+public class DefaultCommand(ILogger<DefaultCommand> logger) : AsyncCommand
+{
+    public override Task<int> ExecuteAsync(CommandContext context)
+    {
+        logger.LogInformation("Default command executed");
+        return Task.FromResult(0);
+    }
+}
+
+public class ExampleCommand(ILogger<ExampleCommand> logger) : AsyncCommand
+{
+    public override Task<int> ExecuteAsync(CommandContext context)
+    {
+        logger.LogInformation("Example command executed");
+        return Task.FromResult(0);
+    }
+}
